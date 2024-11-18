360e2ef8e97fa9a75f26aa89cd74ae6d7f359996
Enda Figueroa <efigueroa@Zipcoders-MacBook-Pro-2.local>
2024-11-18 17:57:29 -0500

    Leap COMPELTE

--- exam1/gregorian_cal_utils.py
+++ exam1/gregorian_cal_utils.py
@@ -1,8 +1,11 @@
 def is_leap_year(year: int) -> bool:
-    """
-    Given a year, this function returns a boolean indicating whether or not it is a leap year.
+    if (year%4 is 0 and year%100 is not 0) or (year%400 is 0):
+                return True
+    return False
+    # """
+    # Given a year, this function returns a boolean indicating whether or not it is a leap year.
 
-    :param year: an integer indicating a year.
-    :return: A boolean indicating whether or not the year parameter is a leap year.
-    """
-    pass  # implement me
+    # :param year: an integer indicating a year.
+    # :return: A boolean indicating whether or not the year parameter is a leap year.
+    # """
+    # pass  # implement me

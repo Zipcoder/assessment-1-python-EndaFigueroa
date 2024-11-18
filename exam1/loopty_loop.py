5ee752f26d296ca3367f5641337eeeba0137a06f
Enda Figueroa <efigueroa@Zipcoders-MacBook-Pro-2.local>
2024-11-18 18:04:53 -0500

    Loops COMPLETE

--- exam1/loopty_loop.py
+++ exam1/loopty_loop.py
@@ -2,25 +2,33 @@ from typing import Callable, List
 
 
 def generate_list(start: int, stop: int, step: int = 1) -> List[int]:
-    """
-    Generate a list of integers.
+    answer = []
+    for i in range(start,stop,step):
+        answer.append(i)
+    return answer
+    # """
+    # Generate a list of integers.
 
-    :param start: Where to start the list (inclusive).
-    :param stop: Where to stop the list (exclusive).
-    :param step: How many digits apart each number is from the others around it.
-    :return: A list of integers.
-    """
-    pass  # implement me
+    # :param start: Where to start the list (inclusive).
+    # :param stop: Where to stop the list (exclusive).
+    # :param step: How many digits apart each number is from the others around it.
+    # :return: A list of integers.
+    # """
+    # pass  # implement me
 
 
 def generate_list_with_strategy(start: int, stop: int, step: int, strategy: Callable) -> List[int]:
-    """
-    Generate a list of integers.
+    answer = []
+    for x in range(start, stop, step):
+        answer.append(strategy(x))
+    return answer
+    # """
+    # Generate a list of integers.
 
-    :param start: Where to start the list (inclusive).
-    :param stop: Where to stop the list (exclusive).
-    :param step: How many digits apart each number is from the others around it.
-    :param strategy: A function to manipulate each digit .
-    :return: A list of integers.
-    """
-    pass  # implement me
+    # :param start: Where to start the list (inclusive).
+    # :param stop: Where to stop the list (exclusive).
+    # :param step: How many digits apart each number is from the others around it.
+    # :param strategy: A function to manipulate each digit .
+    # :return: A list of integers.
+    # """
+    # pass  # implement me

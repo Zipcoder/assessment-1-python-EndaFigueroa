cd3a55a1631803d25c5eddc795ccd1a4c5a0e3eb
Enda Figueroa <efigueroa@Zipcoders-MacBook-Pro-2.local>
2024-11-18 17:39:51 -0500

    Converter COMPLETE

--- exam1/converter.py
+++ exam1/converter.py
@@ -1,48 +1,60 @@
 def meters_to_feet(meters: float) -> float:
-    """
-    This function takes a float representing a measurement in meters
-    and returns the corresponding value converted to feet.
-    The result is rounded to 2 decimal places.
+    feets =0.00
+    feets = meters*3.28084
+    return round(feets,2)
+    # """
+    # This function takes a float representing a measurement in meters
+    # and returns the corresponding value converted to feet.
+    # The result is rounded to 2 decimal places.
 
-    :param meters: A float representing a measurement in meters.
-    :return: A float representing the input measurement converted to feet.
-    """
-    pass  # implement me
+    # :param meters: A float representing a measurement in meters.
+    # :return: A float representing the input measurement converted to feet.
+    # """
+    # pass  # implement me
 
 
 def feet_to_meters(feet: float) -> float:
-    """
-    This function takes a float representing a measurement in feet
-    and returns the corresponding value converted to meters.
-    The result is rounded to 2 decimal places.
+    metees =0.00
+    metees = feet/3.28084
+    return round(metees,2)
+    # """
+    # This function takes a float representing a measurement in feet
+    # and returns the corresponding value converted to meters.
+    # The result is rounded to 2 decimal places.
 
-    :param feet: A float representing a measurement in feet.
-    :return: A float representing the input measurement converted to meters.
-    """
-    pass  # implement me
+    # :param feet: A float representing a measurement in feet.
+    # :return: A float representing the input measurement converted to meters.
+    # """
+    # pass  # implement me
 
 
 def kilometer_to_miles(kilometers: float) -> float:
-    """
-    This function takes a float representing a measurement in kilometers
-    and returns the corresponding value converted to miles.
-    The result is rounded to 2 decimal places.
+    milees = 0.00
+    milees = kilometers*0.621371
+    return round(milees,2)
+    # """
+    # This function takes a float representing a measurement in kilometers
+    # and returns the corresponding value converted to miles.
+    # The result is rounded to 2 decimal places.
 
-    :param kilometers: A float representing a measurement in kilometers.
-    :return: A float representing the input measurement converted to miles.
-    """
-    pass  # implement me
+    # :param kilometers: A float representing a measurement in kilometers.
+    # :return: A float representing the input measurement converted to miles.
+    # """
+    # pass  # implement me
 
 
 def miles_to_kilometers(miles: float) -> float:
-    """
-    This function takes a float representing a measurement in miles
-    and returns the corresponding value converted to kilometers.
-    The result is rounded to 2 decimal places.
+    kmees = 0.00
+    kmees = miles/0.621371
+    return round(kmees,2)
+    # """
+    # This function takes a float representing a measurement in miles
+    # and returns the corresponding value converted to kilometers.
+    # The result is rounded to 2 decimal places.
 
-    :param miles: A float representing a measurement in miles.
-    :return: A float representing the input measurement converted to kilometers.
-    """
-    pass  # implement me
+    # :param miles: A float representing a measurement in miles.
+    # :return: A float representing the input measurement converted to kilometers.
+    # """
+    # pass  # implement me
 
 

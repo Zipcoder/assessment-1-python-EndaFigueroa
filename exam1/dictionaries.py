86a8403166328cdb7382a42afe96560d36e4e5aa
Enda Figueroa <efigueroa@Zipcoders-MacBook-Pro-2.local>
2024-11-18 18:46:48 -0500

    DictCOMPLETE

--- exam1/dictionaries.py
+++ exam1/dictionaries.py
@@ -1,31 +1,59 @@
 
 def delete_keys_from_dict(datadict, keylist):
-    """
-    Delete a list of keys from a dictionary
-    """
-    pass
+    for i in keylist:
+        del datadict[i]
+    return datadict
+
+#### Not passing
+    # """
+    # Delete a list of keys from a dictionary
+    # """
+    # pass
 
 def check_dict_for_key(datadict, key):
-    """
-    Check if a value exists in a dictionary
-    (NO FOR loops!)
-    """
-    pass
+    answer=datadict.values()
+    if key in answer:
+        return True
+    return False
+    # """
+    # Check if a value exists in a dictionary
+    # (NO FOR loops!)
+    # """
+    # pass
 
 def get_key_of_min_value(ddd):
-    """
-    Get the key of the minimum value from a dictionary
-    """
-    pass
+    littleGuy = ddd.values()
+    littleGuy = min(littleGuy)
+    dddKeys = ddd.keys()
+
+    for i in dddKeys:
+        if ddd[i] == littleGuy:
+            return i
+    # """
+    # Get the key of the minimum value from a dictionary
+    # """
+    # pass
 
 def get_key_of_max_value(ddd):
-    """
-    Get the key of the maximum value from a dictionary
-    """
-    pass
+    littleGuy = ddd.values()
+    littleGuy = max(littleGuy)
+    dddKeys = ddd.keys()
+
+    for i in dddKeys:
+        if ddd[i] == littleGuy:
+            return i
+    
+    # """
+    # Get the key of the maximum value from a dictionary
+    # """
+    # pass
 
 def letterfreq(word):
-    '''
-    # Write a function that returns a dictionary of letter frequencies from a word
-    '''
-    pass
+    answer = {}
+    for letter in word:
+        answer[letter] = word.count(letter)
+    return answer
+    # '''
+    # # Write a function that returns a dictionary of letter frequencies from a word
+    # '''
+    # pass
